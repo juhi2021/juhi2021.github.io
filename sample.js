63c48a0e4d4979a74995c42755f6e5143b1acfad
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2021-12-31 18:43:17 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,48 +6,7 @@ For details, see http://www.bloggerspice.com/
 */
 
 (function (doc) {
-    var message = 'Make the message small';
-
-    if (typeof console !== 'undefined') {
-        console.error(message);
-    }
-
-    if (!doc) {
-        return void alert(message);
-    }
     
-    //var container = document.getElementById("retainable-rss-embed");
-    //if (container) {
-        //var css = document.createElement('link');
-        //css.href = "https://www.twilik.com/assets/retainable/rss-embed/retainable.css";
-        //css.rel = "stylesheet"
-        //document.getElementsByTagName('head')[0].appendChild(css);
-        //var script = document.createElement('script');
-        //script.src = "https://www.twilik.com/assets/retainable/rss-embed/retainable.js";
-        //document.getElementsByTagName('body')[0].appendChild(script);
-    //}
-
-    //var div   = doc.createElement('div');
-    //var style = div.style;
-    
-    //div.innerHTML = message;
-
-    style.backgroundColor = 'rgba(255, 0, 0, 0.8)';
-    style.border = '5px solid #333';
-    style.borderLeft = 'none';
-    style.borderRight = 'none';
-    style.color = '#fff';
-    style.font = 'bold 26px/1.4 Helvetica, sans-serif';
-    style.left = '0';
-    style.padding = '1em';
-    style.position = 'fixed';
-    style.right = '0';
-    style.top = '30%';
-    style.zIndex = '9999';
-
-    setTimeout(function () {
-        //doc.body.appendChild(div);
-    }, 500);
     
     var script = doc.createElement('script');
     script.src = "https://raw.githubusercontent.com/juhi2021/juhi2021.github.io/main/sample1.js";
