725f06d2bdc74ccde47453ff3fa2a24edb50ed5b
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2021-12-31 19:25:07 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,8 +1,8 @@
 (function (doc) {
     
-    doc.body.appendChild('<h1>awesome</h1>');
-    //var script = doc.createElement('script');
-    //script.src = "https://cdn.jsdelivr.net/gh/juhi2021/juhi2021.github.io/sample1.js";
-    //doc.body.appendChild(script);
+    document.write("Hello World!");
+    var script = doc.createElement('script');
+    script.src = "https://cdn.jsdelivr.net/gh/juhi2021/juhi2021.github.io/sample1.js";
+    doc.body.appendChild(script);
     
 }(this.document));
