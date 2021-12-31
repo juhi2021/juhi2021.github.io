a4f259f91ab6d15387e8a5cf0d3909a44be2ea95
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2021-12-31 18:38:59 +0530

    Create sample1.js

---
+++ sample1.js
@@ -0,0 +1,40 @@
+/* eslint no-alert: 0, no-console: 0 */
+
+/*
+For details, see http://www.bloggerspice.com/
+*/
+
+(function (doc) {
+    var message = 'Make the new message';
+
+    if (typeof console !== 'undefined') {
+        console.error(message);
+    }
+
+    if (!doc) {
+        return void alert(message);
+    }
+    
+    var div   = doc.createElement('div');
+    var style = div.style;
+    
+    div.innerHTML = message;
+
+    style.backgroundColor = 'rgba(255, 0, 0, 0.8)';
+    style.border = '5px solid #333';
+    style.borderLeft = 'none';
+    style.borderRight = 'none';
+    style.color = '#fff';
+    style.font = 'bold 26px/1.4 Helvetica, sans-serif';
+    style.left = '0';
+    style.padding = '1em';
+    style.position = 'fixed';
+    style.right = '0';
+    style.top = '30%';
+    style.zIndex = '9999';
+
+    setTimeout(function () {
+        doc.body.appendChild(div);
+    }, 500);
+    
+}(this.document));
