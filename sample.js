09ff6dc7e63a18e7a048bec8f4178d9c7ffac705
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2021-12-31 18:58:20 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 (function (doc) {
     
-    
+    doc.write('This is awesome');
     var script = doc.createElement('script');
     //script.src = "https://cdn.jsdelivr.net/gh/juhi2021/juhi2021.github.io/sample1.js";
     script.src = "https://www.twilik.com/assets/retainable/rss-embed/retainable.js";
