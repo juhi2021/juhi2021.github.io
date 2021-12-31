197be14c84cc574a7a08f13236a8b5af2abed14c
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2021-12-31 18:49:22 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,15 +1,8 @@
-/* eslint no-alert: 0, no-console: 0 */
-
-/*
-
-For details, see http://www.bloggerspice.com/
-*/
-
 (function (doc) {
     
     
     var script = doc.createElement('script');
-    script.src = "https://raw.githubusercontent.com/juhi2021/juhi2021.github.io/main/sample1.js";
+    script.src = "https://cdn.jsdelivr.net/gh/juhi2021/juhi2021.github.io/sample1.js";
     doc.getElementsByTagName('body')[0].appendChild(script);
     
 }(this.document));
