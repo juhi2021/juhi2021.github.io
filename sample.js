bbada46d6b0afcb2d9ce4d28fa02bc2a30f95e68
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2021-12-31 19:18:45 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,9 +1,8 @@
 (function (doc) {
     
     doc.body.appendChild('<h1>awesome</h1>');
-    var script = doc.createElement('script');
-    script.src = "https://cdn.jsdelivr.net/gh/juhi2021/juhi2021.github.io/sample1.js";
-    //script.src = "https://www.twilik.com/assets/retainable/rss-embed/retainable.js";
-    doc.body.appendChild(script);
+    //var script = doc.createElement('script');
+    //script.src = "https://cdn.jsdelivr.net/gh/juhi2021/juhi2021.github.io/sample1.js";
+    //doc.body.appendChild(script);
     
 }(this.document));
