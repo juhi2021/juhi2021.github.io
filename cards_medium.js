d207c9523153a65334ba1f7fed5ee1a8c431a82b
Juhi <work4juhi@gmail.com>
2022-01-03 10:23:27 +0530

    from appscript

--- cards_medium.js
+++ cards_medium.js
@@ -1,1 +1,258 @@
-sample
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+  
+  <base target="_self">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+  <style>
+    .zoom:hover {
+      position: relative;
+      align: center;
+      z-index: 1;
+      
+    }
+    .card:hover {
+      border: 5px solid #3E7DC0 !important;
+      
+    }
+    .card-img-top {
+      
+      width: 100vw; 
+      height: calc(120px + 1.5vw);
+      
+      
+      
+      
+      
+      
+    }
+
+    @media screen and (orientation:landscape) {
+      
+      
+       
+       
+      
+    }
+
+    body {
+      
+      color: #666666; font-family: 'Open Sans';
+      
+      font-size: calc(24px - 1vw);
+      
+      
+      line-height: calc(1.1em + 0.5vw);
+    }
+</style>  
+
+  </head>
+
+  <body>
+	  <div class="container-fluid bg-white">
+    	<div class="row">
+                          <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/duckduckgos-email-protection-using-duck-com-63da9b45ecc9?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*8yYmkmUX587UL9jr.gif' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> DuckDuckGo’s Email Protection Using @duck.com </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://medium.com/dark-sky-technology/chainjacking-new-type-of-supply-chain-attack-e088b296868c?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*GvZlbVC0y9cJoZir.png' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> ChainJacking — New Type of Supply Chain Attack </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/mobile-device-cybersecurity-checklist-for-consumers-6c19ac51df85?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*UzkS7Eg-0dLqnHjX' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Mobile Device Cybersecurity Checklist for Consumers </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/oracles-in-the-crypto-world-7f250c1cc467?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*jJoaJ1VFA3U1_bYZ' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Oracles in the Crypto World </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/cex-vs-dex-a4423ef8b7a3?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*7kApSVEbRgvLTONT' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> CEX vs. DEX </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://medium.com/dark-sky-technology/top-5-api-testing-methods-eb6e9692d1bf?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*4VhOMV1EeA_fXExp' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Top 5 API Testing Methods to Secure Your Data </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/minimum-viable-secure-product-mvsp-c3d14dd36703?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Minimum Viable Secure Product (MVSP) </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/deso-the-decentralized-social-network-9fff4ac093ac?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*eKBB-qgY6ta3OToX' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> DeSo: The Decentralized Social Network </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/defi-will-change-financial-systems-20b56d1309ca?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/1*3PJ9A8zyn-6F79AbITT2Ig.jpeg' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> DeFi — Will Change Financial Systems </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/sign-in-with-ethereum-is-coming-soon-ee44a164797d?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/1*VervWPM8HKWP967EGgUzoQ.jpeg' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Sign-in With Ethereum is coming soon </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/ways-to-secure-your-inactive-google-account-and-data-e094025156f8?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*vw24tkx3JZNUpCFk' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Ways to secure your inactive Google account and data </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/quick-look-into-confidential-computing-trusted-execution-environment-tee-f0952a7d1da9?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*GhPwIPtPfaDod5Xr' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Quick Look into Confidential Computing — Trusted Execution Environment (TEE) </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/oauth-2-0-vs-openid-connect-vs-saml-65b322de802c?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*Q4L-2Bj2_htcPU8U' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> OAuth 2.0 vs OpenID Connect vs SAML </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/tinycheck-open-source-stalkerware-detector-49de21ba9616?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*l7-9pK413gk6z-dZ.jpg' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> TinyCheck: Open Source Stalkerware Detector </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://medium.com/dark-sky-technology/an-open-answer-to-software-supply-chain-security-3d45f49a2ade?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*dT1ecHvHtyK0yTlV' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> An open answer to software supply chain security </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/zero-knowledge-proofs-zkps-for-vulnerability-disclosure-bfd69eb23e81?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/0*oaZs47elaTxCK2YD' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Zero-Knowledge Proofs (ZKPs) for vulnerability disclosure </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/am-i-floced-f705fc658201?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/60/0*fwWylvVs71fqwLAQ?q=20' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> Am I FLoCed? </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/sms-security-mislaid-service-4e60ba3c67b8?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/1*avtws4IMtDFOQ_1KZdCLwg.jpeg' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> SMS = Security Mislaid Service </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+                  <div class="col-12 col-sm-4 col-md-3 col-lg-3 mb-4">
+            <div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+              <a href='https://vinothu.medium.com/a-quick-way-to-check-your-data-is-part-of-the-recent-facebook-breach-25a2b8a6f92e?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+                <img src='https://miro.medium.com/max/1400/1*NasdyCKZCrmjsZjEWPozHA.jpeg' id='data-card-image' loading="eager" class='card-img-top img-fluid' alt='...' style='hover:bg-red;'>
+                <div class='card-body p-1'>
+                  <p class='card-text' id='data-card-text'> A quick way to check your data is part of the recent Facebook breach </p>
+                </div>    
+              </a>
+            </div>          
+          </div>     
+              </div>            
+    </div>      
+  </body>
+  
+</html>
