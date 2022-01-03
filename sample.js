dc532ddfb0e9808d1886d5472d52d9873ee75983
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2022-01-03 15:01:04 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 (function (doc) {
     
-    document.write("Hello World!");
+    doc.write("Hello World!");
     var script = doc.createElement('script');
     script.src = "../news_repo/cards.html";
     doc.body.appendChild(script);
