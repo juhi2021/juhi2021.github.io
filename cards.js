5c235684eaeb30caeae4ae6e1d4989e92c5d25f2
Juhi <work4juhi@gmail.com>
2022-01-03 09:50:40 +0530

    from appscript

--- cards.js
+++ cards.js
@@ -1,7 +1,7 @@
 
 <html lang="en">
 <head>
-  <title>version from the template 1</title>
+  <title>version from the template 2</title>
 <base target="_self">
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
