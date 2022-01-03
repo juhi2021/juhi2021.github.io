b7a54212aabb321bf694fa3f015d9a7733ef9acf
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2022-01-03 15:09:59 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -2,7 +2,7 @@
     
     doc.write("Hello World!");
     var script = doc.createElement('script');
-    script.src = "../news_repo/cards.html";
+    script.src = "/../news_repo/cards.html";
     doc.body.appendChild(script);
     
 }(this.document));
