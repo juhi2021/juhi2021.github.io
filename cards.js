2c8170c7a9e828add7e980519dc083ddfc252bea
Juhi <work4juhi@gmail.com>
2022-01-03 06:19:48 +0530

    from appscript

--- cards.js
+++ cards.js
@@ -27,6 +27,123 @@
 </a>
 </div>        <!-- news card -->
 </div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://medium.com/dark-sky-technology/chainjacking-new-type-of-supply-chain-attack-e088b296868c?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/1024/0*GvZlbVC0y9cJoZir.png' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>ChainJacking — New Type of Supply Chain Attack</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>12/16/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://vinothu.medium.com/mobile-device-cybersecurity-checklist-for-consumers-6c19ac51df85?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/1024/0*UzkS7Eg-0dLqnHjX' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>Mobile Device Cybersecurity Checklist for Consumers</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>12/03/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://vinothu.medium.com/oracles-in-the-crypto-world-7f250c1cc467?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/1024/0*jJoaJ1VFA3U1_bYZ' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>Oracles in the Crypto World</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>12/01/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://vinothu.medium.com/cex-vs-dex-a4423ef8b7a3?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/1024/0*7kApSVEbRgvLTONT' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>CEX vs. DEX</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>11/30/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://medium.com/dark-sky-technology/top-5-api-testing-methods-eb6e9692d1bf?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/1024/0*4VhOMV1EeA_fXExp' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>Top 5 API Testing Methods to Secure Your Data</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>11/26/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://vinothu.medium.com/deso-the-decentralized-social-network-9fff4ac093ac?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/1024/0*eKBB-qgY6ta3OToX' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>DeSo: The Decentralized Social Network</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>10/26/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://vinothu.medium.com/defi-will-change-financial-systems-20b56d1309ca?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/800/1*3PJ9A8zyn-6F79AbITT2Ig.jpeg' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>DeFi — Will Change Financial Systems</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>10/20/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://vinothu.medium.com/sign-in-with-ethereum-is-coming-soon-ee44a164797d?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/958/1*VervWPM8HKWP967EGgUzoQ.jpeg' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>Sign-in With Ethereum is coming soon</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>10/18/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
+<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">
+<div class='card text-white bg-white border-secondary rounded h-100 zoom'>
+<a href='https://vinothu.medium.com/ways-to-secure-your-inactive-google-account-and-data-e094025156f8?source=rss-25a06010c94f------2' target='_blank' class='card-link text-secondary' id='data-card-news'>
+<img src='https://cdn-images-1.medium.com/max/1024/0*vw24tkx3JZNUpCFk' id='data-card-image' loading='lazy' class='card-img-top img-fluid' alt='...'>
+<div class='card-body p-1'>
+<p class='card-text' id='data-card-body-text'>Ways to secure your inactive Google account and data</p>
+</div>    <!-- news card body -->
+<div class="card-footer bg-transparent border-0">
+<small class='text-muted'><i class='glyphicon glyphicon-calendar'></i>10/14/2021</small>
+</div>  <!-- news card footer -->
+</a>
+</div>        <!-- news card -->
+</div>   <!-- col card -->
 </div>            <!-- row card -->
 </div>      <!-- container -->
 </body>
