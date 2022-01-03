6095a7291f2ec80176d438d0bc477539062d9491
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2022-01-03 15:19:12 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,8 +1,17 @@
 (function (doc) {
     
-    doc.write("Hello World!");
-    var script = doc.createElement('script');
-    script.src = "/../news_repo/cards.html";
-    doc.body.appendChild(script);
+    doc.write("/../news_repo/cards.html");
+    //var script = doc.createElement('script');
+    //script.src = "/../news_repo/cards.html";
+    //doc.body.appendChild(script);
+    
+    //$(doc).ready(function() {
+    //    $('#myframe').attr('src', 'https://juhi2021.github.io/cards_medium.html');
+    //});
+    
+    jQuery.get('https://juhi2021.github.io/cards_medium.html', function(data) {
+        doc.write(data);
+        //document.getElementById('msgDiv').innerHTML = data;
+    });
     
 }(this.document));
