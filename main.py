3f9739518aea01e874cbbb622ebe763c304bdc29
Juhi Jain <84342287+juhi2021@users.noreply.github.com>
2022-06-23 20:46:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def process_page(xml: str) -> List[List[Union[int, str]]]:
     webpage_parsed = BeautifulSoup(xml, 'lxml-xml')
     webpage_title = webpage_parsed.title
     print(webpage_title)
-    
+
     #create empty dataframe
     #df = pd.DataFrame(columns=['Date Created', 'Title', 'URL', 'Image'])
     write_to_file(convert_html(webpage_parsed))
@@ -53,20 +53,20 @@ def convert_html(webpage):
     lines.append('<div class="row">\n')
 
     for ind, item in enumerate(webpage.findAll('item')):
-      title = item.findChildren('title')
-      title_text = title[0].getText()
-      link = item.findChildren('link')
-      link_text = link[0].getText()
-      date = datetime.strptime(item.findChildren('pubDate')[0].getText(), "%a, %d %b %Y %H:%M:%S %Z")
-      date = datetime.strftime(date.date(), "%m/%d/%Y")
-      image_content = item.findChildren('content:encoded')[0].getText()
-      image_parsed = BeautifulSoup(image_content, 'html.parser')
-      image_src = image_parsed.img['src']
-      print(f"-----------Card values {ind}-----------------")
-      print(date, "\n", title_text, "\n", link_text, "\n", image_src)
-      #df.loc[len(df)] = [date, title_text, link_text, image_src]
+        title = item.findChildren('title')
+        title_text = title[0].getText()
+        link = item.findChildren('link')
+        link_text = link[0].getText()
+        date = datetime.strptime(item.findChildren('pubDate')[0].getText(), "%a, %d %b %Y %H:%M:%S %Z")
+        date = datetime.strftime(date.date(), "%m/%d/%Y")
+        image_content = item.findChildren('content:encoded')[0].getText()
+        image_parsed = BeautifulSoup(image_content, 'html.parser')
+        image_src = image_parsed.img['src']
+        print(f"-----------Card values {ind}-----------------")
+        print(date, "\n", title_text, "\n", link_text, "\n", image_src)
+        #df.loc[len(df)] = [date, title_text, link_text, image_src]
 
-      if ind == 4:
+        #if ind == 4:
         lines.append('<div class="col-12 col-xs-6 col-sm-4 col-md-3 col-lg-3 col-xl-2 mb-4">\n')
         lines.append("<div class='card text-white bg-white border-secondary rounded h-100 zoom'>\n")
         lines.append("<a href='"+link_text+"' target='_blank' class='card-link text-secondary' id='data-card-news'>\n")
@@ -80,9 +80,7 @@ def convert_html(webpage):
         lines.append("</a>\n")
         lines.append("</div>        <!-- news card -->\n")
         lines.append("</div>   <!-- col card -->\n")
-
-      #break
-
+        #break
     lines.append("</div>            <!-- row card -->\n")
     lines.append("</div>      <!-- container -->\n")
     lines.append("</body>\n")
